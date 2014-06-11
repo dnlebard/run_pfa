7106d8058f3663520ef854cb87e2167fbc4a8cb8
David LeBard <dnlebard@marcus.(none)>
2014-06-11 14:12:26 -0400

    (semi) final version of run_pfa.py for cluster analysis

--- run_pfa.py
+++ run_pfa.py
@@ -7,12 +7,12 @@ import shutil
 import sys
 
 ################# ARGUMENT PARSING ##############################
-EXPECTED_ARGS = 12
+EXPECTED_ARGS = 11
 
 num_args = len(sys.argv)
-if num_args < EXPECTED_ARGS:
-    print '\nUsage: %s pfa_exec pfa_inp_file pfa_output_file num_replicas num_proc num_frames data_dir output_dir pfa_inp_dir psf_file nfo_file' % sys.argv[0]
-    print '\nEXAMPLE: %s /Users/dnlebard/code/bin/pfa.cuda.e pfa.inp pfa.out 20 4 200 /Users/dnlebard/cg-peptide-project/pep250/python_run_pfa/data /Users/dnlebard/cg-peptide-project/pep250/python_run_pfa/output/replicas /Users/dnlebard/cg-peptide-project/pep250/pfa-test pep250.psf pep250.nfo\n' % sys.argv[0]
+if num_args != EXPECTED_ARGS:
+    print '\nUsage: %s pfa_exec pfa_inp_file pfa_output_file num_replicas num_proc data_dir output_dir pfa_inp_dir psf_file nfo_file' % sys.argv[0]
+    print '\nEXAMPLE: %s /Users/dnlebard/code/bin/pfa.cuda.e pfa.inp pfa.out 20 4 /Users/dnlebard/cg-peptide-project/pep250/python_run_pfa/data /Users/dnlebard/cg-peptide-project/pep250/python_run_pfa/output/replicas /Users/dnlebard/cg-peptide-project/pep250/pfa-test pep250.psf pep250.nfo\n' % sys.argv[0]
     sys.exit()
 
 pfa_exec   = sys.argv[1]
@@ -20,12 +20,11 @@ pfa_input  = sys.argv[2]
 pfa_output = sys.argv[3]
 num_replicas = int(sys.argv[4])
 num_proc = int(sys.argv[5])
-num_frames = sys.argv[6]
-data_dir =  sys.argv[7] # OLD VERSION: '/Users/dnlebard/cg-peptide-project/pep250/python_run_pfa/data'
-output_base = sys.argv[8] # OLD VERSION:  '/Users/dnlebard/cg-peptide-project/pep250/python_run_pfa/output/replicas'
-pfa_inp_dir = sys.argv[9] # (contains psf file)... OLD VERSION: '/Users/dnlebard/cg-peptide-project/pep250/pfa-test'
-psf_file_name = sys.argv[10]
-nfo_file_name = sys.argv[11]
+data_dir =  sys.argv[6] # OLD VERSION: '/Users/dnlebard/cg-peptide-project/pep250/python_run_pfa/data'
+output_base = sys.argv[7] # OLD VERSION:  '/Users/dnlebard/cg-peptide-project/pep250/python_run_pfa/output/replicas'
+pfa_inp_dir = sys.argv[8] # (contains psf file)... OLD VERSION: '/Users/dnlebard/cg-peptide-project/pep250/pfa-test'
+psf_file_name = sys.argv[9]
+nfo_file_name = sys.argv[10]
 #################################################################
 
 
@@ -36,12 +35,15 @@ cluster_cmd = 'PSF CLUSTER GYR\n'
 cluster_cutoff = '10.0\n'
 cluster_file_out = 'cluster.dat'
 
+num_beads = 20000
+
 for rep_idx in range(num_replicas):
     print '\n\n=========== REPLICA %d =====================' % rep_idx
     print '\nLOOPING OVER REPLICA #%d' % rep_idx
 
     # Make file selection based on the replica id
-    file_by_rep_idx = '*rep' + str(rep_idx) + '*.dcd'
+    file_by_rep_idx = '*rep' + str(rep_idx) + '.*.dcd'
+    print 'THE VALUE OF file_by_rep_idx is "%s"' % file_by_rep_idx
 
     # make a dcd file list
     dcd_file_list = []
@@ -59,7 +61,7 @@ for rep_idx in range(num_replicas):
     #print 'THE CONSTRUCTED OUTPUT dcd FILE: %s\n\n' % output_dcd
 
 
-    str_dcd_file_list = '  '.join(dcd_file_list)
+    str_dcd_file_list = ' '.join(dcd_file_list)
     #print '%s' % ' '.join(dcd_file_list)
 
     # go to output_dir
@@ -73,6 +75,11 @@ for rep_idx in range(num_replicas):
     print 'RUN CATDCD COMMAND .... '
     print 'catdcd command: %s ' % catdcd_cmd
     os.system(catdcd_cmd)
+    get_frames_cmd = '~/bin/catdcd ' + output_dcd + " |grep Total |awk '{print $3}'"
+    #print 'GET FRAMES catdcd command: %s ' % get_frames_cmd
+    num_frames = subprocess.check_output(get_frames_cmd, shell=True)
+    #print 'NEW NUMBER OF FRAMES: %s' % num_frames
+
     print '....DONE!\n'
 
     # Create the nfo file
@@ -89,8 +96,8 @@ for rep_idx in range(num_replicas):
     psf_conf_file.write('0      0\n')
     psf_conf_file.write('0      0\n')
     psf_conf_file.write('0      0\n')
-    psf_conf_file.write('1      5000\n')
-    psf_conf_file.write('5000\n')
+    psf_conf_file.write(('1      ' + str(num_beads) + '\n'))
+    psf_conf_file.write((str(num_beads) + '\n'))
     psf_conf_file.close()
 
     # Create box.in input file
@@ -125,15 +132,13 @@ for rep_idx in range(num_replicas):
     pfa_inp_file.close()
 
     # Make and execute PFA command
-    pfa_cmd = 'mpiexec -np %d %s < %s' % (num_proc,pfa_exec,pfa_input)
-    print 'RUN PFA COMMAND .... '
-    print 'pfa command: %s ' % pfa_cmd
+    #pfa_cmd = 'mpiexec -np %d %s < %s > %s' % (num_proc,pfa_exec,pfa_input,pfa_output)
+    pfa_cmd = 'mpiexec -np %d %s' % (num_proc,pfa_exec)
+    print 'RUNNING PFA COMMAND: |%s < %s >  %s| for replica %d .... ' % (pfa_cmd,pfa_input,pfa_output,rep_idx)
+    subprocess.call(pfa_cmd, stdout=open(pfa_output, 'w'), stdin=open(pfa_input), stderr=subprocess.STDOUT, shell=True)
+    
     print '....DONE!'
 
-    #subprocess.check_call('dos2unix mgo_input', stdout=open('/dev/null','w'), shell=True)
-    #shutil.copy2(mgo_input, 'INPUT')
-    #subprocess.check_call('mpirun -np 8 Pcrystal',
-
     # Remove dcd file
-    os.remove(output_dcd)
+    #os.remove(output_dcd)
     print '\n========================================\n'
